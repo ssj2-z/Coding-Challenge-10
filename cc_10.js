f9b7a098a0eb7a55b00e61fb6eb01a4f5ff4a6f2
ssj2-z <ssj2@usf.edu>
2025-02-20 14:27:23 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -22,3 +22,23 @@ console.log(product1.getDetails());
 product1.updateStock(3);
 console.log(product1.getDetail());
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
+
+// Task 2 Creating Order Class
+class Order {
+    constructor(orderId, product, quantity) {
+        this.orderId = orderId; this.product = product; this.quantity = quantity; this.totalPrice = this.product.price * this.quantity; this.product.updateStock(this.quantity);
+    }
+
+    getOrderDetails() {
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.totalPrice}`;
+    }
+}
+
+// Test Cases
+const order1 = new Order(501, product1, 2);
+console.log(order1.getOrderDetails());
+
+// Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+
+console.log(product1.getDetails());
+// Expected output: "Product: Laptop, ID: 101, Price: 1200, Stock: 5" (Stock reduced)
